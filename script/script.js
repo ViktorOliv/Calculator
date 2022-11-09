957ab5dda74a709f61e13e0d51f4bce24b92dc43
Viktor Or <vitya999123@gmail.com>
2022-11-09 14:47:59 +0200

    add deleteAll

--- script/script.js
+++ script/script.js
@@ -1,5 +1,19 @@
-let firstNumber = " ";
-let lastNumber = " ";
+let firstNumber = ' ',
+    lastNumber =' ',
+    sing = ' ',
+    finish = false;
 
+    const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'], 
+    action = ['-', '+', '*', '/'];
 
-alert ("helloman"); 
+const out = document.querySelector('.line p');
+
+function deleteAll () {
+    firstNumber = ' ';
+    lastNumber = ' ';
+    sing = ' ';
+    finish = false;
+    out.textContent = 0;
+}
+
+document.querySelector('.del button').onclick = deleteAll;
